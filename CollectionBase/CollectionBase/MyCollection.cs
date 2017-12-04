e6031f577c780a9877ed4b9bf354df4b522e66a8
WinterThane <demonsaso@gmail.com>
2017-12-04 11:05:58 +0100

    Collection class
    Added collection class

---
+++ CollectionBase/CollectionBase/MyCollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CollectionBase
+{
+    public class MyCollection : System.Collections.CollectionBase
+    {
+        public void Add(Object item)
+        {
+            InnerList.Add(item);
+        }
+
+        public void Remove(Object item)
+        {
+            InnerList.Remove(item);
+        }
+
+        public new int Count()
+        {
+            return InnerList.Count;
+        }
+
+        public new void Clear()
+        {
+            InnerList.Clear();
+        }
+    }
+}
--- CollectionBase/CollectionBase/Program.cs
+++ CollectionBase/CollectionBase/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CollectionBase
 {
@@ -10,6 +6,26 @@ namespace CollectionBase
     {
         static void Main(string[] args)
         {
+            var names = new MyCollection();
+            names.Add("Marko");
+            names.Add("Ana");
+            names.Add("Tadej");
+            names.Add("Mojca");
+
+            foreach (Object name in names)
+            {
+                Console.WriteLine(name);
+                
+            }
+            Console.WriteLine("Number of names: " + names.Count());
+
+            names.Remove("Marko");
+            Console.WriteLine("Number of names: " + names.Count());
+
+            names.Clear();
+            Console.WriteLine("Number of names: " + names.Count());
+
+            Console.ReadLine();
         }
     }
 }
