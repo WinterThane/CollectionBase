055914a2d94a77efa78de7d861492e19f4ee9ac3
WinterThane <demonsaso@gmail.com>
2017-12-04 16:06:24 +0100

    Timer class
    Added timer class for diagnostics

--- CollectionBase/CollectionBase/Program.cs
+++ CollectionBase/CollectionBase/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace CollectionBase
 {
@@ -6,24 +7,36 @@ namespace CollectionBase
     {
         static void Main(string[] args)
         {
-            var names = new MyCollection();
-            names.Add("Marko");
-            names.Add("Ana");
-            names.Add("Tadej");
-            names.Add("Mojca");
+            var names = new MyCollection
+            {
+                "Marko",
+                "Ana",
+                "Tadej",
+                "Mojca"
+            };
 
             foreach (Object name in names)
             {
-                Console.WriteLine(name);
-                
+                Console.WriteLine(name);               
             }
             Console.WriteLine("Number of names: " + names.Count());
-
             names.Remove("Marko");
             Console.WriteLine("Number of names: " + names.Count());
-
             names.Clear();
             Console.WriteLine("Number of names: " + names.Count());
+
+            Console.WriteLine();
+
+            int arrayLength = 99999;
+            int[] numbers = new int[arrayLength];
+            numbers = UtilFunctions.BuildArray(arrayLength);
+            Timing timer = new Timing();
+
+            timer.StartTimer();
+            UtilFunctions.DisplayNums(numbers);
+            timer.StopTime();
+
+            Console.WriteLine("Duration: {0} in milliseconds.", timer.Result().TotalMilliseconds);
 
             Console.ReadLine();
         }
---
+++ CollectionBase/CollectionBase/UtilFunctions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CollectionBase
+{
+    public class UtilFunctions
+    {
+        public static int[] BuildArray(int length)
+        {
+            int[] array = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = i;
+            }
+
+            return array;
+        }
+
+        public static void DisplayNums(int[] array)
+        {
+            for (int i = 0; i <= array.GetUpperBound(0); i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
